b3d108966e7727ef3f23e3e34ea9da127425b563
Harsh <harsh@rarecrew.com>
2020-10-30 10:09:44 +0530

    FIX: Added sql connection

--- Application/Source/server.js
+++ Application/Source/server.js
@@ -11,3 +11,52 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => winston.info(`server running on port ${port}`));
 
 app.response.write("It's alive!");
+
+// const dboperation = require('./dboperation');
+// var order = require('./order');
+
+
+// dboperation.getUser().then(result => {
+//     console.log(result);
+// })
+const sql = require('mssql');
+
+const config = {
+	user: 'dbadmin',
+	password: 'NkW03T7&C&YLkvjlK7EQ',
+	server: 'money-manager-db.database.windows.net',
+	database: 'money-manager'
+}
+
+function test() {
+	try {
+		sql.connect(config).then(pool => {
+			pool.request()
+				// .input('input_parameter', sql.Int, value)
+				.query('select * from Persons').then(res => {
+					console.dir(res)
+
+				})
+		})
+
+
+
+		// Stored procedure
+
+		// let result2 = await pool.request()
+		//     .input('input_parameter', sql.Int, value)
+		//     .output('output_parameter', sql.VarChar(50))
+		//     .execute('procedure_name')
+
+		// console.dir(result2)
+	} catch (err) {
+		// ... error checks
+	}
+}
+
+sql.on('error', err => {
+	// ... error handler
+})
+
+test();
+
